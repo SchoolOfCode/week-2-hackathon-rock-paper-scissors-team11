d06b4380b2d902a31f244fec2774b333489ea900
Hope <hopeivy@hotmail.co.uk>
2024-12-05 14:48:44 +0000

    first verison - needs correction

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let score= 0;
-let playAgain = true;
+let score = 0;
+let playAgain = false; 
 
 // Build array of "Rock", "Paper" and "Scissors" ✅
 const game = [
@@ -7,10 +7,14 @@ const game = [
     "paper",
     "scissors"
 ]
+
+//let usersGuess = prompt ("Guess between Rock, Paper and Scissors").toLowerCase();
 console.log (game)
 // Build a loop to randomly select between three options
-while (playAgain == true); {
-    const generatedChoice = Math.floor(Math.random() * game.length);
+//function rockPaperScissors (score, playAgain, game){
+
+    while (playAgain) {
+    const generatedChoice = game [Math.floor(Math.random() * game.length)];
     let usersGuess = prompt ("Guess between Rock, Paper and Scissors").toLowerCase();
     
     //if (usersGuess == generatedChoice); {
@@ -18,15 +22,28 @@ while (playAgain == true); {
     //} else {}
 
     switch (true) {
-        case userGuess == generatedChoice:
+        case usersGuess == generatedChoice:
             alert ("It's a Tie!");
             break;
-            case
+            case usersGuess == "scissors" && generatedChoice == "paper":
+            case usersGuess == "rock" && generatedChoice == "scissors":
+            case usersGuess == "paper" && generatedChoice == "rock":
+            alert (`${generatedChoice} You win`);
+            break;
+            default:
+            alert (`${generatedChoice} You loss, try again!`);
+   }
+     playAgain = confirm("Would you like to play again?");
+     
     }
-}
+       // }
 
+//rockPaperScissors(score, playAgain, game);
+//playAgain = confirm("Would you like to play again?");
 
-console.log (generatedChoice)
+//console.log (generatedChoice);
+
+console.log(game[2]);
 // Build prompt to user to select between three options
 // Build if else statements to compare three options chosen by computer and user
 // Build alerts for winning and losing
