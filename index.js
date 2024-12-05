ed88bd3f6acff53350b54bf897d2960411be4cf1
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-05 11:51:44 +0000

    Completed array
    Started work on loop, and testing each individual
    part as we go.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let score= 0;
+let playAgain = true;
 
 // Build array of "Rock", "Paper" and "Scissors" ✅
 const game = [
@@ -8,6 +9,14 @@ const game = [
 ]
 console.log (game)
 // Build a loop to randomly select between three options
+//while (playAgain == true); {
+    const generatedChoice = Math.floor(Math.random() * game.length);
+    let usersGuess = prompt ("Guess between Rock, Paper and Scissors", (""));
+    if (usersGuess == generatedChoice);
+//}
+
+
+console.log (generatedChoice)
 // Build prompt to user to select between three options
 // Build if else statements to compare three options chosen by computer and user
 // Build alerts for winning and losing
