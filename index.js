7db7d9b3c57465ec8f31619bb183ae6c685a783e
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-05 11:25:14 +0000

    Added index and java file
    Created a array for the rock, paper, scissors game.

---
+++ index.js
@@ -0,0 +1,14 @@
+let score= 0;
+
+// Build array of "Rock", "Paper" and "Scissors" ✅
+const game = [
+    "Rock",
+    "Paper",
+    "Scissors"
+]
+console.log (game)
+// Build a loop to randomly select between three options
+// Build prompt to user to select between three options
+// Build if else statements to compare three options chosen by computer and user
+// Build alerts for winning and losing
+// Build play again option to continue game or game over.
